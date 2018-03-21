99b7ee0e891802d34d63fe09b51d9fa76ba43f3d
Roland Tecson <roland_tecson@hotmail.com>
2018-03-20 20:58:49 -0700

    Initial Commit

---
+++ w6d3-demo-1/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  w6d3-demo-1
+//
+//  Created by Roland on 2018-03-20.
+//  Copyright © 2018 MoozX Internet Ventures. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
